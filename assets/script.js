422f2b83a0b51a3f6f9e54d65385368f23250836
ZLandon <zacharylandonps@gmail.com>
2022-01-04 18:19:57 -0500

    Cleaned up small bits of code Fixed issue when values outside of the acceptable range are entered into the second prompt Removed spaces from password character sets

--- assets/script.js
+++ assets/script.js
@@ -2,33 +2,36 @@
 //needed global values
 var optionOne = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 var optionTwo = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-var optionThree = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz !#$%&'()*+,-./:;<=>?@[]^_`{|}~\"";
-var optionFour = "0123456789 !#$%&'()*+,-./:;<=>?@[]^_`{|}~\"";
-var optionFive = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !#$%&'()*+,-./:;<=>?@[]^_`{|}~\"";
+var optionThree = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[]^_`{|}~\"";
+var optionFour = "0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~\"";
+var optionFive = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[]^_`{|}~\"";
 
 var options = [optionOne, optionTwo, optionThree, optionFour, optionFive];
 
-var promptChar = '';
-var numNeeded = promptChar;
+var promptChar, numNeeded = '';
+var charSelectPrompt;
 
 //working to clean up cases
-function getText(optionNum) {
-    if (!(optionNum > 0 && optionNum < 6)) {
+function getText(charSelectPrompt) {
+    numNeeded = '';
+    if (!(charSelectPrompt > 0 && charSelectPrompt < 6)) {
         window.alert("You did not pick a valid option. Try again.");
 
             //call generator agin so user can pick valid option
             generatePassword();
     }
-    for(var i = 0; i < promptChar; i++) {
-        var randomNumber = Math.floor(Math.random() * options[optionNum-1].length);
-        numNeeded += options[optionNum-1].substring(randomNumber, randomNumber + 1);
+    else {
+        for(var i = 0; i < promptChar; i++) {
+            var randomNumber = Math.floor(Math.random() * options[charSelectPrompt-1].length);
+            numNeeded += options[charSelectPrompt-1].substring(randomNumber, randomNumber + 1);
+        }
     }
 }
 
 // generator code here
 function charLength() {
     //ask how many characters are needed
-    promptChar = window.prompt("How many characters would you like to generate for you password? /nPlease choose from 8 to 128.");
+    promptChar = window.prompt("How many characters would you like to generate for you password? \nPlease choose from 8 to 128.");
 
     promptChar = parseInt(promptChar);
 
@@ -42,13 +45,12 @@ function charLength() {
 
 function generatePassword() {
     //ask what types of characters they would like for their password
-    var charSelectPrompt = window.prompt("Which characters would you like to include in your password? \nPlease enter one of the following: 1 for just LETTERS A-Z, 2 for LETTERS and NUMBERS, 3 for LETTERS and SPECIAL CHARACTERS, 4 for NUMBERS and SPECIAL CHARACTERS, 5 for ALL options.")
+    charSelectPrompt = window.prompt("Which characters would you like to include in your password? \nPlease enter one of the following: 1 for just LETTERS A-Z, 2 for LETTERS and NUMBERS, 3 for LETTERS and SPECIAL CHARACTERS, 4 for NUMBERS and SPECIAL CHARACTERS, 5 for ALL options.")
 
     charSelectPrompt = parseInt(charSelectPrompt);
     getText(charSelectPrompt);
     return numNeeded;
 }
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -56,10 +58,9 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
     var passwordText = document.querySelector("#password");
-    document.querySelector("#password").value;
-    var password = charLength();
+    passwordText.value = "";
 
-    passwordText.value = password;
+    passwordText.value = charLength();
 }
 
 // Add event listener to generate button
